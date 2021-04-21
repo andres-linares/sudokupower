b15e0dc5b5c6703d8a2740076d41f22f1b94b633
andres-linares <andres.linares.971122@gmail.com>
2021-04-21 00:17:05 -0500

    fix: integer in range has now a uniform distribution

--- src/utils/Random.ts
+++ src/utils/Random.ts
@@ -1,8 +1,8 @@
 export class Random {
   static intInRange(min: number, max: number): number {
-    const difference = max - min;
-    const randomValue = Math.round(Math.random() * difference);
+    const difference = max - min + 1;
+    const randomValue = Math.floor(Math.random() * difference + min);
 
-    return randomValue + min;
+    return randomValue;
   }
 }
--- tests/utils/Random.spec.ts
+++ tests/utils/Random.spec.ts
@@ -7,4 +7,23 @@ describe('randomIntInRange', () => {
       expect(value >= 1 && value <= i).toEqual(true);
     }
   });
+
+  it.only('should have a uniform distribution', () => {
+    const values = [];
+    for (let i = 0; i < 100; i++) {
+      values.push(0);
+    }
+
+    const TRIES = 10_000;
+
+    for (let i = 0; i < TRIES; i++) {
+      const value = Random.intInRange(0, 99);
+      values[value]++;
+    }
+
+    const distribution = values.map((value) => value / TRIES);
+    expect(distribution.every((value) => value >= 0.0065 && value <= 0.0135)).toEqual(
+      true
+    );
+  });
 });
