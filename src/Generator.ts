53ee244cd211afe6d11c5e656d4e3fe74ef2a78f
andres-linares <andres.linares.971122@gmail.com>
2021-04-21 13:49:01 -0500

    feat: implement better generation algorithm

--- src/Generator.ts
+++ src/Generator.ts
@@ -1,5 +1,6 @@
 import { Cell } from './Cell';
 import { Board } from './utils/Board';
+import { Logger } from './utils/Logger';
 import { Random } from './utils/Random';
 import { Utils } from './utils/Utils';
 
@@ -18,22 +19,42 @@ export class Generator {
   }
 
   generateSolvedGame(): Cell[][] {
+    const MAX_TRIES_PER_ROW = 10;
+    const MAX_TRIES_PER_QUADRANT = 5;
+
+    let triesPerRow = 0;
+    let triesPerQuadrant = 0;
+
     for (let row = 0; row < 9; row++) {
       this.cells.push([]);
 
       for (let column = 0; column < 9; column++) {
         const freeInRow = Board.freeInRow(row, this.cells);
         const freeInColumn = Board.freeInColumn(column, this.cells);
-        const availables = Utils.intersection(freeInColumn, freeInRow);
+        const freeInQuadrant = Board.freeInQuadrant(row, column, this.cells);
+        const availables = Utils.intersection(freeInColumn, freeInRow, freeInQuadrant);
 
-        const value = Random.pick(availables);
-        if (!value) {
-          this.cells = this.cells.splice(0, row);
-          row--;
+        if (availables.length === 0) {
+          if (triesPerRow < MAX_TRIES_PER_ROW) {
+            this.cells = this.cells.slice(0, row);
+            row = row - 1;
+            triesPerRow += 1;
+          } else if (triesPerQuadrant < MAX_TRIES_PER_QUADRANT) {
+            this.cells = this.cells.slice(0, row - (row % 3));
+            row = row - (row % 3) - 1;
+            triesPerRow = 0;
+            triesPerQuadrant += 1;
+          } else {
+            this.cells = [];
+            row = -1;
+            triesPerRow = 0;
+            triesPerQuadrant = 0;
+          }
 
           break;
         }
 
+        const value = Random.pick(availables);
         this.cells[row].push(new Cell(value));
       }
     }
