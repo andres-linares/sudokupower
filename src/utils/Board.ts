11094d91c22217e916e275e3999bb2326e4d1b09
andres-linares <andres.linares.971122@gmail.com>
2021-04-21 10:28:03 -0500

    feat: implement freeInQuadrant method

--- src/utils/Board.ts
+++ src/utils/Board.ts
@@ -16,4 +16,42 @@ export class Board {
 
     return ALL_NUMBERS.filter((number) => !values.includes(number));
   }
+
+  static freeInQuadrant(
+    rowIndex: number,
+    columnIndex: number,
+    cells: Cell[][]
+  ): number[] {
+    const quadrant = this.cellsInQuadrant(rowIndex, columnIndex, cells);
+    const values = quadrant.map((cell) => cell?.value);
+
+    return ALL_NUMBERS.filter((number) => !values.includes(number));
+  }
+
+  private static cellsInQuadrant(row: number, column: number, cells: Cell[][]): Cell[] {
+    if (row <= 2 && column <= 2) return this.cellsAround(1, 1, cells);
+    if (row <= 2 && column <= 5) return this.cellsAround(1, 4, cells);
+    if (row <= 2) return this.cellsAround(1, 7, cells);
+
+    if (row <= 5 && column <= 2) return this.cellsAround(4, 1, cells);
+    if (row <= 5 && column <= 5) return this.cellsAround(4, 4, cells);
+    if (row <= 5) return this.cellsAround(4, 7, cells);
+
+    if (column <= 2) return this.cellsAround(7, 1, cells);
+    if (column <= 5) return this.cellsAround(7, 4, cells);
+
+    return this.cellsAround(7, 7, cells);
+  }
+
+  private static cellsAround(row: number, column: number, cells: Cell[][]): Cell[] {
+    const around: Cell[] = [];
+
+    for (let i = row - 1; i <= row + 1; i++) {
+      for (let j = column - 1; j <= column + 1; j++) {
+        around.push(cells[i][j]);
+      }
+    }
+
+    return around;
+  }
 }
--- tests/src/utils/Board.spec.ts
+++ tests/src/utils/Board.spec.ts
@@ -88,3 +88,63 @@ describe('freeInColumn', () => {
     expect(result).toEqual([3, 4, 6, 7, 8]);
   });
 });
+
+describe('freeInQuadrant', () => {
+  it('should return all numbers when quadrant is empty', () => {
+    let cells: Cell[][] = [
+      [C(), C(), C()],
+      [C(), C(), C()],
+      [C(), C(), C()],
+    ];
+    let result = Board.freeInQuadrant(0, 0, cells);
+    expect(result).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+  });
+
+  it('should return only available numbers', () => {
+    let cells: Cell[][] = [
+      [C(1), C(), C()],
+      [C(), C(), C()],
+      [C(), C(), C()],
+    ];
+    let result = Board.freeInQuadrant(0, 0, cells);
+    expect(result).toEqual([2, 3, 4, 5, 6, 7, 8, 9]);
+
+    cells = [
+      [C(1), C(), C()],
+      [C(), C(), C()],
+      [C(), C(), C(3)],
+    ];
+    result = Board.freeInQuadrant(1, 1, cells);
+    expect(result).toEqual([2, 4, 5, 6, 7, 8, 9]);
+
+    cells = [
+      [C(1), C(), C(7)],
+      [C(), C(), C()],
+      [C(4), C(), C(3)],
+    ];
+    result = Board.freeInQuadrant(2, 2, cells);
+    expect(result).toEqual([2, 5, 6, 8, 9]);
+
+    cells = [
+      [C(), C(), C(), C(), C(), C(), C(1), C(2), C(3)],
+      [C(), C(), C(), C(), C(), C(), C(4), C(), C(5)],
+      [C(), C(), C(), C(), C(), C(), C(8), C(7), C(6)],
+    ];
+    result = Board.freeInQuadrant(2, 8, cells);
+    expect(result).toEqual([9]);
+
+    cells = [
+      [C(), C(), C(), C(), C(3), C(), C(), C(), C()],
+      [C(), C(), C(), C(), C(), C(), C(), C(), C()],
+      [C(), C(), C(), C(), C(), C(), C(), C(), C()],
+      [C(), C(), C(), C(), C(), C(), C(), C(4), C()],
+      [C(), C(), C(), C(), C(), C(), C(), C(), C()],
+      [C(), C(1), C(), C(), C(), C(), C(), C(), C()],
+      [C(), C(), C(), C(), C(7), C(), C(), C(), C()],
+      [C(), C(), C(), C(), C(), C(), C(), C(), C()],
+      [C(), C(), C(), C(), C(2), C(), C(), C(), C()],
+    ];
+    result = Board.freeInQuadrant(8, 5, cells);
+    expect(result).toEqual([1, 3, 4, 5, 6, 8, 9]);
+  });
+});
